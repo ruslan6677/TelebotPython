13599ecedff9515fc00f1182216c0376d8c1be79
RuslanBTB <87086724+ruslan6677@users.noreply.github.com>
2022-12-13 10:09:31 +0400

    Add files via upload

---
+++ telebot.py
@@ -0,0 +1,26 @@
+from html.parser import HTMLParser
+import telegram.ext
+import telegram
+
+TOKEN = "5741026234:AAF3yWufod-JqfuXkeH1cGvpvIfhKU7LANQ"
+
+def start(update,source):
+    update.message.reply_text("<b>Merhaba, Hoşgeldiniz.\nWeb Sitesi:/site\nYoutube:/youtube</b>",parse_mode = telegram.ParseMode.HTML)
+
+
+def site(update,source):
+    update.message.reply_text("<b>Kusha Mühendislik Web Sitesi : https://kushaengineering.com </b>",parse_mode = telegram.ParseMode.HTML)
+
+def video(update,source):
+    update.message.reply_text("<b>Kusha Mühendislik Youtube : https://www.youtube.com/watch?v=Gt_cwST1Nhg </b>",parse_mode = telegram.ParseMode.HTML,disable_web_page_preview=True)
+
+update = telegram.ext.Updater(TOKEN,use_context=True)
+disp = update.dispatcher
+
+disp.add_handler(telegram.ext.CommandHandler("start",start))
+disp.add_handler(telegram.ext.CommandHandler("site",site))
+disp.add_handler(telegram.ext.CommandHandler("youtube",video))
+
+update.start_polling()
+update.idle()
+
